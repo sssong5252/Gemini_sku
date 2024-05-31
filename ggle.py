4c1dbe944aeb86e6b2aadcd0d9fb753fbe3f9ae4
sssong5252 <luckyjude@naver.com>
2024-05-31 12:58:01 +0900

    gemini api put history

---
+++ ggle.py
@@ -0,0 +1,56 @@
+"""
+Proprietary License
+
+Copyright (c) 2024 [JUNYONG-SONG]
+
+All rights reserved. Unauthorized copying, use, or distribution of this code is strictly prohibited without prior written permission from the author.
+
+For permission requests, contact: [luckyjude@sungkyul.ac.kr]
+"""
+
+
+import google.generativeai as genai
+
+GOOGLE_API_KEY=""
+
+genai.configure(api_key=GOOGLE_API_KEY)
+
+model = genai.GenerativeModel('gemini-pro')
+
+history = [
+    {
+        "role": "user",
+        "parts": [
+            {"text": "내 이름은 준용이야 기억해줘"}
+        ]
+    },
+    
+    {
+        "role": "user",
+        "parts": [
+            {"text": "A의 이름은 우현이야 기억해줘"}
+        ]
+    },
+    #데이터 추가 가능
+]
+
+chat = model.start_chat(history=history)
+
+while True:
+    message_text = input("입력: ")
+
+    if message_text == "exit":
+        break
+
+    message = {
+        "role": "user",
+        "parts": [
+            {"text": message_text}
+        ]
+    }
+
+    response = chat.send_message(message)
+    print("응답:")
+    for part in response.parts:
+        if hasattr(part, "text"):
+            print(part.text)
